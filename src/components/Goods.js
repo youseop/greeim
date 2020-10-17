6f208bfe69eb2aa1aa8f7967df03306d9c9b96d4
Youseop <66366941+youseop@users.noreply.github.com>
2020-10-18 00:09:24 +0900

    .

---
+++ src/components/Goods.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { dbService, storageService } from "../fbase";
+
+const Goods = ({ GoodsObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newGoods, setNewGoods] = useState(GoodsObj.text);
+    const onDeleteClick = async () => {
+      const ok = window.confirm("Are you sure you want to delete?");
+      if (ok) {
+        await dbService.doc(`Design/${GoodsObj.id}`).delete();
+        await storageService.refFromURL(GoodsObj.attachmentUrl).delete();
+      }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      await dbService.doc(`Design/${GoodsObj.id}`).update({
+        text: newGoods,
+      });
+      setEditing(false);
+    };
+    const onChange = (event) => {
+      const {
+        target: { value },
+      } = event;
+      setNewGoods(value);
+    };
+    return (
+      <div>
+        {editing ? (
+          <>
+            <form onSubmit={onSubmit}>
+              <input
+                type="text"
+                placeholder="Edit"
+                value={newGoods}
+                required
+                autoFocus
+                onChange={onChange}
+              />
+              <input type="submit" value="Update explanation"/>
+            </form>
+            <span onClick={toggleEditing}>
+              Cancel
+            </span>
+          </>
+        ) : (
+          <>
+            <h4>{GoodsObj.text}</h4>
+            {GoodsObj.GoodsattachmentUrl && <img src={GoodsObj.GoodsattachmentUrl} />}
+            {isOwner && (
+              <div>
+                <span onClick={onDeleteClick}>
+                    Delete
+                </span>
+                <span onClick={toggleEditing}>
+                    Edit
+                </span>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    );
+  };
+  
+export default Goods;
--- src/routes/Design.js
+++ src/routes/Design.js
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from "react";
+import Goods from "../components/Goods";
 import { dbService } from "../fbase";
 
 const Design = ({ userObj }) => {
-
+    const [designs, setdesigns] = useState([]);
+    useEffect(() => {
+      dbService
+        .collection("Design")
+        .orderBy("createdAt", "desc")
+        .onSnapshot((snapshot) => {
+          const DesignArray = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setdesigns(DesignArray);
+        });
+    }, []);
     return (
-        <div>Design</div>
-
+      <div>
+        <div style={{ marginTop: 30 }}>
+          {designs.map((goods) => (
+            <Goods
+              key={goods.id}
+              GoodsObj={goods}
+              isOwner={goods.creatorId === userObj.uid}
+            />
+          ))}
+        </div>
+      </div>
     );
-};
+  };
 export default Design;
